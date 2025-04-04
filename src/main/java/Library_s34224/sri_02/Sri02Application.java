a5172b536f4fb6ea27b6dbe6f258f0c493361e91
Mariusz Januszek <mariusz.januszek95@gmail.com>
2025-04-04 12:32:04 +0200

    Initial commit Java Spring

---
+++ src/main/java/Library_s34224/sri_02/Sri02Application.java
@@ -0,0 +1,13 @@
+package Library_s34224.sri_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sri02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sri02Application.class, args);
+	}
+
+}
---
+++ src/test/java/Library_s34224/sri_02/Sri02ApplicationTests.java
@@ -0,0 +1,13 @@
+package Library_s34224.sri_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sri02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
